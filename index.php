b0f832eebb4780bb867d8944fb76f048d1f5aff6
ignacio864 <ignaciocortazzo.rr@gmail.com>
2026-03-21 19:10:01 -0300

    Funcionalidad 'Ver la lista de menú en la Homepage' y archivos con la estructura de la DB y juego de datos de prueba

--- index.php
+++ index.php
@@ -5,124 +5,43 @@
         <section class="menu">
             <h2>Nuestro Menú</h2>
             
-            <!-- Entrantes -->
-            <div class="menu-category">
-                <h3>Entrantes</h3>
-                <div class="menu-items">
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Ensalada de la Casa</h4>
-                            <span class="price">$300</span>
-                        </div>
-                        <p class="description">Lechuga, tomate, cebolla, queso y aderezo casero</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Tabla de Quesos y Embutidos</h4>
-                            <span class="price">$320</span>
-                        </div>
-                        <p class="description">Selección de quesos importados y jamón ibérico</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Camarones al Ajillo</h4>
-                            <span class="price">$435</span>
-                        </div>
-                        <p class="description">Camarones frescos salteados con ajo y perejil</p>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Platos Principales -->
-            <div class="menu-category">
-                <h3>Platos Principales</h3>
-                <div class="menu-items">
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Filete de Res</h4>
-                            <span class="price">$790</span>
-                        </div>
-                        <p class="description">Corte premium a la parrilla, acompañado de papas y vegetales</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Salmón a la Mantequilla</h4>
-                            <span class="price">$990</span>
-                        </div>
-                        <p class="description">Salmón fresco con salsa de mantequilla y limón</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Pechuga de Pollo Rellena</h4>
-                            <span class="price">$780</span>
-                        </div>
-                        <p class="description">Pechuga rellena de queso y jamón, acompañada de arroz</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Pasta Fettuccine</h4>
-                            <span class="price">$648</span>
-                        </div>
-                        <p class="description">Pasta fresca con salsa de crema y parmesano</p>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Postres -->
-            <div class="menu-category">
-                <h3>Postres</h3>
-                <div class="menu-items">
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Tiramisú</h4>
-                            <span class="price">$490</span>
-                        </div>
-                        <p class="description">Postre italiano clásico con café y mascarpone</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Brownie LOCO</h4>
-                            <span class="price">$330</span>
-                        </div>
-                        <p class="description">Brownie casero con chocolate</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Flan de Caramelo</h4>
-                            <span class="price">$350</span>
-                        </div>
-                        <p class="description">Suave y cremoso con caramelo casero</p>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Bebidas -->
-            <div class="menu-category">
-                <h3>Bebidas</h3>
-                <div class="menu-items">
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Refrescos</h4>
-                            <span class="price">$180</span>
-                        </div>
-                        <p class="description">Coca-Cola, Sprite, Fanta y más</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Vinos Seleccionados</h4>
-                            <span class="price">$600 - $3900</span>
-                        </div>
-                        <p class="description">Vinos tintos y blancos importados</p>
-                    </div>
-                    <div class="menu-item">
-                        <div class="item-header">
-                            <h4>Cervezas Artesanales</h4>
-                            <span class="price">$230</span>
-                        </div>
-                        <p class="description">Selección de cervezas locales e internacionales</p>
-                    </div>
-                </div>
-            </div>
+            <?php
+            include 'db.php';
+            
+            $sql_cat = "SELECT * FROM Categoria ORDER BY id";
+            $result_cat = $conn->query($sql_cat);
+            
+            if ($result_cat->num_rows > 0) {
+                while($cat = $result_cat->fetch_assoc()) {
+                    echo '<div class="menu-category">';
+                    echo '<h3>' . htmlspecialchars($cat['nombre']) . '</h3>';
+                    echo '<div class="menu-items">';
+                    
+                    $sql_items = "SELECT * FROM MenuItems WHERE categoria_id = " . $cat['id'];
+                    $result_items = $conn->query($sql_items);
+                    
+                    if ($result_items->num_rows > 0) {
+                        while($item = $result_items->fetch_assoc()) {
+                            echo '<div class="menu-item">';
+                            echo '<div class="item-header">';
+                            echo '<h4>' . htmlspecialchars($item['nombre']) . '</h4>';
+                            echo '<span class="price">$' . $item['precio'] . '</span>';
+                            echo '</div>';
+                            echo '</div>';
+                        }
+                    } else {
+                        echo '<p>No hay items en esta categoría.</p>';
+                    }
+                    
+                    echo '</div>';
+                    echo '</div>';
+                }
+            } else {
+                echo '<p>No hay categorías disponibles.</p>';
+            }
+            
+            $conn->close();
+            ?>
         </section>
     </main>
 
